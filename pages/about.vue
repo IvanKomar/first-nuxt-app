ebe7900969f4854c5a297ff306005a34f147c4f6
Ivan Komar <komar.ioan@gmail.com>
2020-02-08 03:02:20 +0200

    finished first nuxt app

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+    <section>
+        <h1>About page</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aliquid iure natus beatae fugit tempore harum earum, dolore voluptates voluptas labore deleniti, aspernatur possimus at rem reprehenderit consectetur dicta reiciendis.</p>
+    </section>
+</template>
+
+<script>
+export default {
+   middleware: ['auth']
+}
+</script>
---
+++ pages/users/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <nuxt-link to='/users'>Go back to users</nuxt-link>
+    <h1>
+     {{user.name}}
+    </h1>
+    <hr>  
+    <h3>{{user.email}}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  validate({params}) {
+     return /^\d+$/.test(params.id)
+  },
+  async asyncData({$axios, params}){
+    const user = await $axios.$get('https://jsonplaceholder.typicode.com/users/' + params.id)
+    return {user}
+  }
+
+}
+</script>
